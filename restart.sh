4dcf237323c8b898bb109fdd5676b11085e1194c
Aaron Lenoir <lenoir.aaron@gmail.com>
2018-04-12 23:53:11 +0000

    Update theme when running up.

---
+++ restart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./down.sh
+./up.sh
--- up.sh
+++ up.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+echo "Updating custom casper theme ..."
+git -C blog/data/ghost/themes/casper-custom pull
+echo "Running the containers (detached) ..."
 sudo docker-compose up -d
